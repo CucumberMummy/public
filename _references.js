d2af5728f2ad212346a245d0b1f1074520fc7e4b
noname <noname@email.com>
2017-01-08 17:58:42 +0700

    Initial commit

---
+++ _references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="lib/jquery/jquery.cookie.js" />
+/// <reference path="lib/jquery/jquery-3.1.1.min.js" />
+/// <reference path="lib/jquery/jquery-modified.min.js" />
